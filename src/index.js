6285474cc9b5022fbe4dc538cd6164482eddb5d8
djag477 <101579022+djag477@users.noreply.github.com>
2022-06-25 16:13:18 +0200

    browserouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    
+      <App />
+    
+  </BrowserRouter>
+));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
